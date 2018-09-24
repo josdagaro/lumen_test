f6b352437d53dd8a3b98e7422b2a5350f81abc29
José David <josdagaro@gmail.com>
2018-09-24 17:31:38 -0500

    Seeder has been added

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,9 +11,9 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Hotel::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->email,
+        'location' => $faker->address,
     ];
 });
--- database/seeds/HotelsTableSeeder.php
+++ database/seeds/HotelsTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class HotelsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,8 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            HotelsTableSeeder::class,
-        ]);
+        factory(App\Hotel::class, 50)->create();
     }
 }
